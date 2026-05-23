ed7d940e052e33d160056b27a4c35686638da915
CEO of the Society of Anti Brainrot People <markhamonca@codeninjas.com>
2026-05-23 15:50:00 -0400

    Codey Raceway - Step 72

---
+++ Kris Yu - Codey Raceway/Assets/Scripts/checkpointCollision.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkpointCollision : MonoBehaviour
+{
+    public bool didCollide; //true or false variable thingy
+
+    void Start()
+    {
+        didCollide = false; //at the start, set it to false
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && didCollide == false) //check if codey collided with the game object and if the didCollide variable is false
+        {
+            didCollide = true; //if both conditions are met, set didCollide to true
+        }
+    }
+}
